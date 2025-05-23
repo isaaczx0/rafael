24fd645cca29bde3f0ac5d360743808de57a0fe9
isaaczx0 <isaacpb@unipam.edu.br>
2025-05-23 00:29:11 +0000

    aula 22/05/25

---
+++ aulasla/script.js
@@ -0,0 +1,7 @@
+function calcular(){
+    soma.value = parseint(valor1.value) + parseint(valor2.value);
+    sub.value = parseint(valor1.value) - parseint(valor2.value);
+    divisao.value = parseint(valor1.value) / parseint(valor2.value);
+    mult.value = parseint(valor1.value) * parseint(valor2.value);
+}
+    
